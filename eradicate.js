aa1f4ad38d38cff7cec024bf9c448b3ce5df9f52
deemeetree <info@noduslabs.com>
2014-04-13 05:26:17 +0200

    added new instructions

--- eradicate.js
+++ eradicate.js
@@ -56,23 +56,35 @@ var quoteList = [
         source: "reload page for another advice"
     },
     {
-        quote: "Check the profile of somebody you haven't seen in a year.",
+        quote: "There are people who <a href='https://www.facebook.com/search/me/residents-near/present'>live just a few minutes away</a> - maybe you could visit one of them...",
         source: "reload page for another advice"
     },
     {
-        quote: "Check out what people post in the groups that Newsfeed never shows you.",
+        quote: "There are groups, which are <a href='https://www.facebook.com/browsegroups/?category=nearby'>popular nearby</a> - maybe join of them?",
         source: "reload page for another advice"
     },
     {
-        quote: "Who do you never see in this newsfeed? Check their profile!",
+        quote: "Check the profile of <a href='https://www.facebook.com/search/me/users-followed'>somebody</a> you don't usually see.",
         source: "reload page for another advice"
     },
     {
-        quote: "What about that message that you never replied to? Maybe now is the time to do it?",
+        quote: "Check out what people post in <a href='https://www.facebook.com/browsegroups/?category=membership'>the groups that Newsfeed never shows you</a>.",
         source: "reload page for another advice"
     },
     {
-        quote: "Make introduction between the two people who are different but may have something in common.",
+        quote: "Who do you never see in this newsfeed? <a href='https://www.facebook.com/search/me/residents-near/present'>Check their profile</a>!",
+        source: "reload page for another advice"
+    },
+    {
+        quote: "What about <a href='https://www.facebook.com/messages/'>that message</a> that you never replied to? Maybe now is the time to do it?",
+        source: "reload page for another advice"
+    },
+    {
+        quote: "Find a new <a href='https://www.facebook.com/search/me/non-friends/me/friends/friends/intersect'>friend of friend</a>.",
+        source: "reload page for another advice"
+    },
+    {
+        quote: "Make introduction between <a href='https://www.facebook.com/search/me/friends/photos-of/likers/me/friends/intersect'>the two friends</a> who are different but may have something in common.",
         source: "reload page for another advice"
     },
     {
@@ -89,6 +101,10 @@ var quoteList = [
     },
     {
         quote: "Visit a website you really like and you haven't visited for a while.",
+        source: "reload page for another advice"
+    },
+    {
+        quote: "Maybe it's better to visit <a href='http://soundcloud.com'>soundcloud</a> or open your <a href='http://tumblr.com'>tumblr</a> instead?",
         source: "reload page for another advice"
     },
     {
