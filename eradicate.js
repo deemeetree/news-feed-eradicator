6910875f05f295c375a9bd0cd90ce282747f8ad7
deemeetree <info@noduslabs.com>
2014-04-13 03:34:39 +0200

    added new quotes

--- eradicate.js
+++ eradicate.js
@@ -1,23 +1,7 @@
 var quoteList = [ 
 	{
-		quote: "Do not let the Ghetto pull you back. But do let it give you strength and power to escape into Cosmos. Sometimes we need the opposing force to be stronger.",
-		source: "a ghetto wisdom"
-	},
-	{
-		quote: "It's good to be in the center of things when you know what you have to offer. It's also good to be in the center of things to know what you can offer. But then what you offer will be very much related to what the center demands...",
-		source: "a friendly advice"
-	},
-	{
 		quote: "If we don't discipline ourselves, the world will do it for us on its own terms.",
 		source: "William Feather, remixed"
-	},
-	{
-		quote: "Rule your mind or it will rule you. Let your mind rule you or you will have to rule it yourself. Rule out any necessity to rule and start steering instead.",
-		source: "Horace, chopped & screwed"
-	},
-	{
-		quote: "All that we are is the result of what we have thought. What we have thought, however, is the result of what we are. Nonequilibrium stability of one's own existence is based on this feedback loop.",
-		source: "Buddha, remixed"
 	},
 	{
 		quote: "Every particle is exploding and emploding at every moment of time.",
@@ -25,59 +9,95 @@ var quoteList = [
 	},
 	{
 		quote: "The most plausible mechanism for large-scale integration is the formation of dynamic links mediated by synchrony over multiple frequency bands.",
-		source: "Francisco Varela"
+		source: "<a href='http://www.nature.com/nrn/journal/v2/n4/abs/nrn0401_229a.html' target='_blank'>Varela et al 2001</a>"
 	},
 	{
 		quote: "Time flows, things change. Their natural tendency to disappear is appreciated. Patterns emerge.",
-		source: "an archiving enthusiast"
-	},
-	{
-		quote: "Summer follows the spring. Smoking kills. Objects in mirrors may appear smaller. Trespassers will be persecuted. Cause always follows the effect.",
-		source: "an obscure authority"
+		source: "<a href='http://polysingularity.com/archive/' target='_blank'>Polysingularity</a>"
 	},
 	{
 		quote: "Protect. Control. Survive. Wash obsessively. Stop taking drugs. Eat vegan. Use condoms. Be efficient. Be tolerant. Be polite. Be interesting. Be somebody else.",
-		source: "an inner voice from the outside"
-	},
-	{
-		quote: "I can try to accept failure, everyone fails at something. But I can't try to accept not trying.",
-		source: "Michael Jordan, remastered"
-	},
-	{
-		quote: "There’s a myth that time is money. In fact, time is more precious than money. Once you’ve spent it, it’s gone forever. However, time has the capacity to stretch, warp, and form circular patterns. Therefore just like one can change one's perception of time, one can also change one's perception of money.",
-		source: "Neil A. Fiore, screwed"
+		source: "<a href='http://polysingularity.com/recommendations/' target='_blank'>Polysingularity</a>"
 	},
 	{
 		quote: "It is easy to inspect an object, to determine which properties it has, by attempting to retrieve the properties and examining the values obtained. ",
-		source: "a book on JavaScript"
-	},
-	{
-		quote: "There is only one success--to be able to spend your life in your own way.",
-		source: "Christopher Morley"
-	},
-	{
-		quote: "Success is the good fortune that comes from aspiration, desperation, transformation, dedication, perspiration, inspiration, information, revelation, creation, abstraction, penetration, commitment, and will.",
-		source: "Evan Esar, modified"
-	},
-	{
-		quote: "We are still masters of our fate. We are still captains of our souls.",
-		source: "Winston Churchill"
+		source: "<a href='http://7chan.org/pr/src/OReilly_JavaScript_The_Good_Parts_May_2008.pdf' target='_blank'>JavaScript, The Good Parts</a>"
 	},
 	{
 		quote: "The best way to make your dreams come true is to wake up and dream lucid.",
 		source: "Paul Valery, remastered"
 	},
     {
-        quote: "The difference between a successful person and others is not a lack of strength, not a lack of knowledge, but rather a lack in will.",
-        source: "Vince Lombardi Jr."
-    },
-    {
         quote: "True freedom is impossible without a mind made free by discipline.",
-        source: "Mortimer J. Adler"
+        source: "Mortimer J. Adler, original"
     },
     {
         quote: "The most powerful control we can ever attain, is to be in control of ourselves while releasing it at the same time occasionally.",
         source: "Chris Page, remastered"
+    },
+    {
+        quote: "People commit a forecasting error by expecting to feel better after Facebook use.",
+        source: "<a href='http://www.sciencedirect.com/science/article/pii/S0747563214001241' target='_blank'>Sagioglou & Greitemeier, 2014</a>"
+    },
+    {
+        quote: "Facebook activity, but not internet browsing negatively correlates with mood.",
+        source: "<a href='http://www.sciencedirect.com/science/article/pii/S0747563214001241' target='_blank'>Sagioglou & Greitemeier, 2014</a>"
+    },
+    {
+        quote: "A feeling of having wasted time accounts for the effect of Facebook activity on mood.",
+        source: "<a href='http://www.sciencedirect.com/science/article/pii/S0747563214001241' target='_blank'>Sagioglou & Greitemeier, 2014</a>"
+    },
+    {
+        quote: "Using chance or intuition one may reach a point on the plane which also belongs to another plane, a totally different class of solutions. The creative leap is the ability to recognize this transition point and to jump from one class of solutions to another.",
+        source: "<a href='http://bit.ly/1oXyn9F' target='_blank'>Noy et al, 2012</a>"
+    },
+    {
+        quote: "If your mind is wandering, take a look in the window.",
+        source: "reload page for another advice"
+    },
+    {
+        quote: "Check the profile of somebody you haven't seen in a year.",
+        source: "reload page for another advice"
+    },
+    {
+        quote: "Check out what people post in the groups that Newsfeed never shows you.",
+        source: "reload page for another advice"
+    },
+    {
+        quote: "Who do you never see in this newsfeed? Check their profile!",
+        source: "reload page for another advice"
+    },
+    {
+        quote: "What about that message that you never replied to? Maybe now is the time to do it?",
+        source: "reload page for another advice"
+    },
+    {
+        quote: "Make introduction between the two people who are different but may have something in common.",
+        source: "reload page for another advice"
+    },
+    {
+        quote: "Uhm... maybe 5 minutes of exercises instead?",
+        source: "reload page for another advice"
+    },
+    {
+        quote: "Don't forget to blink and to shift the focus – it's good for your eyes (and brain).",
+        source: "reload page for another advice"
+    },
+    {
+        quote: "Maybe call a friend you haven't seen for a while instead?",
+        source: "reload page for another advice"
+    },
+    {
+        quote: "Visit a website you really like and you haven't visited for a while.",
+        source: "reload page for another advice"
+    },
+    {
+        quote: "Walk away for 30 seconds and come back to the screen. Maybe you'll want to do something else.",
+        source: "reload page for another advice"
+    },
+    {
+        quote: "Maybe 3 minutes of meditation instead?",
+        source: "reload page for another advice"
     }
 ];
 
